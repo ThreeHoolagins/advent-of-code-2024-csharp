28e88ce013786ef3f2d8d8e26d83920ed2385b83
ThreeHoolagins <cbcunningham20@gmail.com>
2024-12-03 00:27:42 -0600

    attempt of day 2

--- AdventOfCode2024/Controllers/AdventTesterController.cs
+++ AdventOfCode2024/Controllers/AdventTesterController.cs
@@ -24,5 +24,41 @@ namespace AdventOfCode2024.Controllers
             var _dayOneService = new DayOneService();
             return _dayOneService.dayOneChallengePart2(input.firstListOfLocationIds, input.secondListOfLocationIds);
         }
+
+        [HttpGet]
+        [Route("DayTwo")]
+        public int dayTwoAction()
+        {
+            var _dayTwoService = new DayTwoService();
+            return _dayTwoService.dayTwoChallenge(getDay2Data());
+        }
+
+        [HttpGet]
+        [Route("DayTwoPart2")]
+        public int dayTwoPart2Action()
+        {
+            var _dayTwoService = new DayTwoService();
+            return _dayTwoService.dayTwoPart2Challenge(getDay2Data());
+        }
+
+        #region Private Methods
+
+        private List<List<int>> getDay2Data()
+        {
+            List<List<int>> lines = new List<List<int>>();
+            string line;
+            StreamReader sr = new StreamReader("C:\\Users\\Caleb\\source\\repos\\advent-of-code-2024-csharp\\AdventOfCode2024\\SampleData\\Day2Data.txt");
+            line = sr.ReadLine();
+            lines.Add(line?.Split([' ']).Select(int.Parse).ToList());
+            while (line != null)
+            {
+                lines.Add(line?.Split([' ']).Select(int.Parse).ToList());
+                line = sr.ReadLine();
+            }
+
+            return lines;
+        }
+
+        #endregion
     }
 }
---
+++ AdventOfCode2024/Services/DayTwoService.cs
@@ -0,0 +1,159 @@
+﻿
+namespace AdventOfCode2024.Services
+{
+    public class DayTwoService
+    {
+        public int dayTwoChallenge(List<List<int>> inputData)
+        {
+            return dayTwoChallengeFirstSolve(inputData);
+        }
+
+        public int dayTwoPart2Challenge(List<List<int>> inputData)
+        {
+            return dayTwoChallengePart2FirstSolve(inputData);
+            //return dayTwoChallengePart2SecondSolve(inputData);
+        }
+
+        private int dayTwoChallengeFirstSolve(List<List<int>> inputData)
+        {
+            int passedReports = 0;
+            foreach (var list in inputData)
+            {
+                int direction = Math.Sign(list[0] - list[1]);
+                if (direction == 0)
+                {
+                    continue;
+                }
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    int difference = list[i] - list[i + 1];
+                    if (Math.Abs(difference) > 3 || (Math.Sign(difference) != Math.Sign(direction)))
+                    {
+                        break;
+                    }
+
+                    if (i == list.Count - 2) 
+                    {
+                        passedReports++;
+                    }
+                }
+            }
+
+            if (passedReports != 0)
+            {
+                return passedReports;
+            }
+
+            // Fail case
+            return -1;
+        }
+
+        private int dayTwoChallengePart2FirstSolve(List<List<int>> inputData)
+        {
+            int newlyPassingReports = 0;
+            foreach (var list in inputData)
+            {
+                int direction = Math.Sign(list[0] - list[1]);
+
+                if (direction == 0)
+                {
+                    continue;
+                }
+
+                int mistakes = 0;
+
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    int difference = list[i] - list[i + 1];
+                    if (Math.Abs(difference) > 3 || (Math.Sign(difference) != Math.Sign(direction)))
+                    {
+                        if (mistakes == 0)
+                        {
+                            mistakes++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    // We no longer care for the normal passing reports
+                    if (i == list.Count - 2)
+                    {
+                        newlyPassingReports++;
+                    }
+                }
+            }
+
+            if (newlyPassingReports != 0)
+            {
+                return newlyPassingReports;
+            }
+
+            // Fail case
+            return -1;
+        }
+
+        private int dayTwoChallengePart2SecondSolve(List<List<int>> inputData)
+        {
+            int newlyPassingReports = 0;
+            foreach (var list in inputData)
+            {
+                int direction = Math.Sign(list[0] - list[1]);
+
+                if (direction == 0)
+                {
+                    continue;
+                }
+
+                int mistakePosition = 0;
+
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    int difference = list[i] - list[i + 1];
+                    if (Math.Abs(difference) > 3 || (Math.Sign(difference) != Math.Sign(direction)))
+                    {
+                        mistakePosition = i+1;
+                        break;
+                    }
+
+                    if (i == list.Count - 2)
+                    {
+                        newlyPassingReports++;
+                    }
+                }
+
+                if (mistakePosition > 0)
+                {
+                    list.RemoveAt(mistakePosition);
+                    int retryDirection = Math.Sign(list[0] - list[1]);
+                    if (retryDirection == 0)
+                    {
+                        continue;
+                    }
+                    for (int i = 0; i < list.Count - 1; i++)
+                    {
+                        int difference = list[i] - list[i + 1];
+                        if (Math.Abs(difference) > 3 || (Math.Sign(difference) != Math.Sign(retryDirection)))
+                        {
+                            break;
+                        }
+
+                        if (i == list.Count - 2)
+                        {
+                            newlyPassingReports++;
+                        }
+                    }
+                }
+            }
+
+            if (newlyPassingReports != 0)
+            {
+                return newlyPassingReports;
+            }
+
+            // Fail case
+            return -1;
+        }
+    }
+}
